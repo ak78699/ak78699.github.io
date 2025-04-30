8e34a689b370832853ce53f74781a8b00b903731
Aira <ak7869@nyu.edu>
2025-04-30 15:35:56 +0400

    Test Font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,15 @@ export default {
 	darkMode: "class",
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#f5f4ee', // beige: '#f5f4ee'
+				secondary:'#d6b594'  //light brown
+			},
+			fontFamily: {
+                'inknut': ['Inknut Antiqua', 'serif'], // Added Playfair Display
+            },
+		},
 	},
 	plugins: [require("@tailwindcss/typography")],
 };
